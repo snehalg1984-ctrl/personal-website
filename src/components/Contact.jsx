99b125572ffd0e3bdf2f58ed179f35d4afe2e262
snehalg1984-ctrl <snehalg1984@gmail.com>
2026-05-16 12:47:05 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
     <section id="contact">
       <h2>Contact Me</h2>
       <p>Email: yourmail@gmail.com</p>
-      <p>Phone: 1234567890</p>
+      <p>Phone: 12345678909</p>
     </section>
   );
 }
